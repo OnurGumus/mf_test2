e3f499181382b188a9ef64bc1a0cbc893f8e2455
Onur Gumus <onurgumus@microsoft.com>
2021-08-08 13:50:28 +0000

    reverseproxy

--- src/Web/Program.fs
+++ src/Web/Program.fs
@@ -10,80 +10,146 @@ open Microsoft.Extensions.Logging
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 open DotLiquid
+open Yarp.ReverseProxy.Forwarder
+open System.Net.Http
+open FSharp.Control.Tasks
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Http
 
 // ---------------------------------
 // Web app
 // ---------------------------------
 
-let indexHandler (name : string) =
+let indexHandler (name: string) =
     let greetings = sprintf "Hello %s, from Giraffe!" name
-    let model     = {| Text = greetings |}
+    let model = {| Text = greetings |}
     dotLiquidHtmlTemplate "Views/Index.html" model
 
+let getAuth () = ()
+//  let client = HttpClient
 let webApp =
-    choose [
-        GET >=>
-            choose [
-                route "/" >=> indexHandler "world"
-                routef "/hello/%s" indexHandler
-            ]
-        setStatusCode 404 >=> text "Not Found" ]
+    choose [ GET
+             >=> choose [ route "/" >=> indexHandler "world"
+                          routef "/hello/%s" indexHandler ]
+             setStatusCode 404 >=> text "Not Found" ]
 
 // ---------------------------------
 // Error handler
 // ---------------------------------
 
-let errorHandler (ex : Exception) (logger : ILogger) =
+let errorHandler (ex: Exception) (logger: ILogger) =
     logger.LogError(ex, "An unhandled exception has occurred while executing the request.")
-    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+    clearResponse
+    >=> setStatusCode 500
+    >=> text ex.Message
 
 // ---------------------------------
 // Config and Main
 // ---------------------------------
 
-let configureCors (builder : CorsPolicyBuilder) =
+let configureCors (builder: CorsPolicyBuilder) =
     builder
-        .WithOrigins(
-           "http://0.0.0.0:5000","https://0.0.0.0:5001")
-       .AllowAnyMethod()
-       .AllowAnyHeader()
-       |> ignore
+        .WithOrigins("http://0.0.0.0:5000", "https://0.0.0.0:5001")
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+    |> ignore
 
-let configureApp (app : IApplicationBuilder) =
-    let env = app.ApplicationServices.GetService<IWebHostEnvironment>()
+type CustomTransformer() =
+    inherit HttpTransformer()
+
+    override _.TransformRequestAsync(httpContext, proxyRequest, destinationPrefix) =
+        let tr =
+            base.TransformRequestAsync(httpContext, proxyRequest, destinationPrefix)
+
+        task {
+            do! tr
+            proxyRequest.Headers.Host <- null
+        }
+        |> ValueTask
+
+let configureApp (app: IApplicationBuilder) =
+    let forwarder =
+        app.ApplicationServices.GetService<IHttpForwarder>()
+
+    let httpClient =
+        new HttpMessageInvoker(
+            new SocketsHttpHandler(
+                UseProxy = false,
+                AllowAutoRedirect = false,
+                AutomaticDecompression = System.Net.DecompressionMethods.None,
+                UseCookies = false
+            )
+        )
+
+    let transformer = CustomTransformer() // or HttpTransformer.Default;
+
+    let requestOptions =
+        ForwarderRequestConfig(Timeout = TimeSpan.FromSeconds(100.))
+
+    let env =
+        app.ApplicationServices.GetService<IWebHostEnvironment>()
+
+    app
+        .UseRouting()
+        .UseEndpoints(fun endpoints ->
+            endpoints.Map(
+                "/{**catch-all}",
+                RequestDelegate
+                    (fun httpContext ->
+                        upcast (task {
+                                    let! _ =
+                                        forwarder.SendAsync(
+                                            httpContext,
+                                            "https://auth:5011/",
+                                            httpClient,
+                                            requestOptions,
+                                            transformer
+                                        )
+
+                                    return ()
+                                }))
+            )
+            |> ignore
+
+
+            )
+    |> ignore
+
     (match env.IsDevelopment() with
-    | true  ->
-        app.UseDeveloperExceptionPage()
-    | false ->
-        app .UseGiraffeErrorHandler(errorHandler)
-            .UseHttpsRedirection())
+     | true -> app.UseDeveloperExceptionPage()
+     | false ->
+         app
+             .UseGiraffeErrorHandler(errorHandler)
+             .UseHttpsRedirection())
         .UseCors(configureCors)
         .UseStaticFiles()
         .UseGiraffe(webApp)
 
-let configureServices (services : IServiceCollection) =
-    services.AddCors()    |> ignore
-    services.AddGiraffe() |> ignore
+let configureServices (services: IServiceCollection) =
+    services.AddHttpForwarder().AddCors().AddGiraffe()
+    |> ignore
 
-let configureLogging (builder : ILoggingBuilder) =
-    builder.AddConsole()
-           .AddDebug() |> ignore
+let configureLogging (builder: ILoggingBuilder) =
+    builder.AddConsole().AddDebug() |> ignore
 
 [<EntryPoint>]
 let main args =
     let contentRoot = Directory.GetCurrentDirectory()
-    let webRoot     = Path.Combine(contentRoot, "wwwroot")
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(
-            fun webHostBuilder ->
-                webHostBuilder
-                    .UseContentRoot(contentRoot)
-                    .UseWebRoot(webRoot)
-                    .UseUrls("http://0.0.0.0:5000","https://0.0.0.0:5001")
-                    .Configure(Action<IApplicationBuilder> configureApp)
-                    .ConfigureServices(configureServices)
-                    .ConfigureLogging(configureLogging)
-                    |> ignore)
+    let webRoot = Path.Combine(contentRoot, "wwwroot")
+
+    Host
+        .CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(fun webHostBuilder ->
+            webHostBuilder
+                .UseContentRoot(contentRoot)
+                .UseWebRoot(webRoot)
+                .UseUrls("http://0.0.0.0:5000", "https://0.0.0.0:5001")
+                .Configure(Action<IApplicationBuilder> configureApp)
+                .ConfigureServices(configureServices)
+                .ConfigureLogging(configureLogging)
+            |> ignore)
         .Build()
         .Run()
+
     0
