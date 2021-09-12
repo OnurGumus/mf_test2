9bd07b87652881b99913ad01679e92838689fc45
Onur Gumus <onurgumus@microsoft.com>
2021-09-12 08:26:53 +0000

    shadowdom

--- src/App/backend/wwwroot/my-component.js
+++ src/App/backend/wwwroot/my-component.js
@@ -1,7 +1,47 @@
-class MyComponent extends HTMLElement {
-    connectedCallback() {
-      this.innerHTML = `<h1>Hello world</h1>`;
+class MenuToggle extends HTMLElement {
+  constructor() {
+    super();
+
+
+    // check for a Declarative Shadow Root:
+    let shadow = this.shadowRoot;
+    if(!shadow)
+    {
+      this.polyFill();
     }
+    console.log(shadow);
+    // this.internals = this.attachInternals();
+
+    // // check for a Declarative Shadow Root:
+    // let shadow = this.internals.shadowRoot;
+    // if (!shadow) {
+    //   // there wasn't one. create a new Shadow Root:
+    //   // shadow = this.attachShadow({mode: 'open'});
+    //   // shadow.innerHTML = `<button><slot></slot></button>`;
+    // }
+    // else {
+
+
+
+    // }
   }
-      
-  customElements.define('my-component', MyComponent);
+  polyFill(){
+    
+                this.querySelectorAll('template[shadowroot]').forEach(template => {
+                const mode = template.getAttribute('shadowroot');
+                if(!template.parentNode.shadowRoot){
+                const shadowRoot = template.parentNode.attachShadow({ mode });
+                shadowRoot.appendChild(template.content);
+                template.remove();
+                }
+                });
+        }
+  connectedCallback() {
+    let shadow = this.shadowRoot;
+    const toggle = e => { console.log("toggled") };
+    // in either case, wire up our event listener:
+    shadow.querySelector('button').addEventListener('click', toggle);
+    console.log('Custom square element added to page.');
+  }
+}
+customElements.define('menu-toggle', MenuToggle);
